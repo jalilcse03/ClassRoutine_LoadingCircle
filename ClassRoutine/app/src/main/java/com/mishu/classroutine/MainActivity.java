cbbb0e47d5d4d52dbbb5599af1cd026d941caab8
jalilcse03 <jalil_cse@yahoo.com>
2016-02-16 01:36:10 +0600

    testing
    testing

--- ClassRoutine/app/src/main/java/com/mishu/classroutine/MainActivity.java
+++ ClassRoutine/app/src/main/java/com/mishu/classroutine/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+            System.out.print("ok");
 
 
         circleFill = (CircleFillView) findViewById(R.id.circleFillView);
